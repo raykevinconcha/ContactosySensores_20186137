f736dbfea8734f24697e3b257fbf899a33421ed9
raykevinconcha <a20186137@pucp.edu.pe>
2023-10-01 17:41:06 -0500

    enlaces

--- app/src/main/java/com/example/contactosysensores/Acelerometro.java
+++ app/src/main/java/com/example/contactosysensores/Acelerometro.java
@@ -2,7 +2,10 @@ package com.example.contactosysensores;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Acelerometro extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Acelerometro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acelerometro);
+
+        Button irAcelerometroButton = findViewById(R.id.button6);
+        irAcelerometroButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Acelerometro.this,Magnetometro.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/contactosysensores/Magnetometro.java
+++ app/src/main/java/com/example/contactosysensores/Magnetometro.java
@@ -2,7 +2,10 @@ package com.example.contactosysensores;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Magnetometro extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Magnetometro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_magnetometro);
+
+        Button irAcelerometroButton=findViewById(R.id.button3);
+        irAcelerometroButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Magnetometro.this,Acelerometro.class);
+                startActivity(intent);
+            }
+        });
     }
 }
