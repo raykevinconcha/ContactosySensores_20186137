5fe42a4d0f080498e8e1b6bdb0efd88c95d8f748
raykevinconcha <a20186137@pucp.edu.pe>
2023-10-01 12:14:39 -0500

    Initial commit

---
+++ app/src/main/java/com/example/contactosysensores/Magnetometro.java
@@ -0,0 +1,14 @@
+package com.example.contactosysensores;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Magnetometro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_magnetometro);
+    }
+}
